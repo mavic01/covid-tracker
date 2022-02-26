dc8a7507cc0029bcc731c542977c32438fe8959f
Victor Iberi <iberivictor7@gmail.com>
2022-02-26 14:34:53 +0100

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="text-center bg-green-800 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3">
+      <i class="fa fa-viruses"></i> Mavic's Covid-19 Tracker
+    </div>
+
+    <p>API by <a href="https://covid19api.com" target="_blank" class="text-green-300">covid19api.com</a></p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
+    darkMode: false,
+    theme: { extend: {}, container: { center: true, padding: '2rem', },},
+    variants: { extend: {}, },
+    plugins: [],
+}
